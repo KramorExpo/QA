9efad5b1f20c0f63c40124a78e6a8b1b85f87ca6
Kramor <kramorexpo@icloud.com>
2020-09-19 02:26:38 +0300

    init def
    Была создана функция, принимающая на вход class и тип искомого элемента

--- Selenium+python/parsingDTFTitle.py
+++ Selenium+python/parsingDTFTitle.py
@@ -2,9 +2,23 @@ from selenium import webdriver
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 driver.get('https://dtf.ru/')
-titles = driver.find_elements_by_class_name("content-header__title")
-for title in titles:
-    print(title.text)
+
+def return_elements_by_class_and_type(className, type):
+    result = []
+    elementsList = driver.find_elements_by_class_name(className)
+    if type == "text":
+         for element in elementsList:
+            result.append(element.text)
+    if type == "href":
+        for element in elementsList:
+            result.append(element.get_attribute('href'))
+
+    return result
+
+print(return_elements_by_class_and_type("content-header__title", "text"))
+print(return_elements_by_class_and_type("content-feed__link", "href"))
+
+driver.close()
 
 
 
